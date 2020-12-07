7cc40056c85270ff823be7070d4123373d0cfce7
Ankesh <ankeshagrawal440@gmail.com>
2020-12-07 21:06:12 +0530

    Added Static root

--- nsp_project/settings.py
+++ nsp_project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR=os.path.join(BASE_DIR,'template')
-STATIC_DIR=os.path.join(BASE_DIR,'staticfiles')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,3 +126,9 @@ USE_TZ = True
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
