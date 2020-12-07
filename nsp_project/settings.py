95d81cafa16ae8edaaa67c9401cf3cd4cd0f114c
Ankesh <ankeshagrawal440@gmail.com>
2020-12-07 20:44:34 +0530

    Added Coresheader

--- nsp_project/settings.py
+++ nsp_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'nsp_project_app',
+    'corsheaders',
     'django_filters',
 ]
 
@@ -123,6 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 MEDIA_URL= '/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media/'
-)
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 STATIC_URL = '/static/'
