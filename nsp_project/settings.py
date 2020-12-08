c8a2281f6ed2d986dd2957120e270cef74d1146a
Ankesh01 <70638498+Ankesh01@users.noreply.github.com>
2020-12-08 21:00:50 +0530

    Update settings.py

--- nsp_project/settings.py
+++ nsp_project/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR=os.path.join(BASE_DIR,'template')
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
